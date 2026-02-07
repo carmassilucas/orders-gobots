28ce313deb51d123fb7243440589cc1c9d0d89ea
carma <carmassilucas@gmail.com>
2026-02-07 11:02:09 -0300

    chore: base project generated by initializr

---
+++ marketplace-api/src/main/kotlin/ai/gobots/marketplace_api/MarketplaceApiApplication.kt
@@ -0,0 +1,11 @@
+package ai.gobots.marketplace_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketplaceApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MarketplaceApiApplication>(*args)
+}
